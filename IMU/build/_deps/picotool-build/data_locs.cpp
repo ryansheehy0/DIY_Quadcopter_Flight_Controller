54b1a250e0209fd218b75f472954a83b1122f609
ryan <ryansheehy0@gmail.com>
2025-08-24 19:13:56 -0700

    Stop tracking build folder

--- IMU/build/_deps/picotool-build/data_locs.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> data_locs = {
-    "./","/media/ryan/250GB/Learning/DIY_Quadcopter_Flight_Controller/IMU/build/_deps/picotool/"
-};
